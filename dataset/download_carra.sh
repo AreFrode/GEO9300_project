a073d1f46cefd14ec3cd8c459e6716a6abd88c80
Are Frode Kvanum <arefk@met.no>
2025-09-24 16:49:48 +0200

    Updated gif with reanalysis data

---
+++ dataset/download_carra.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+year=2025
+
+echo "downloading Carra"
+
+atosdir="/ec/res4/scratch/nor1878/carra_retrievals/${year}/"
+localdir="/home/arefk/phd/geo9300/GEO9300_project/dataset/CARRA/${year}/"
+rsync_options="-avz"
+
+for MM in {05..07}; do
+  targetdir="${localdir}${MM}/"
+  mkdir -p "$targetdir"
+  
+  echo "Syncing month ${MM} from ECMWF HPC"
+  if rsync $rsync_options hpc-login:"${atosdir}${MM}/" "$targetdir"; then
+    echo "Successfully synced month ${MM} for year ${year}"
+  fi
+
+done
