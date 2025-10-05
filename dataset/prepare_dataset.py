9593c6bb797904c03b75b8e4d50c7b92296af455
Are Frode Kvanum <arefk@met.no>
2025-10-05 17:25:01 +0200

    Finished dataset prepare script
    SIC is a new column for all buoys, sic is based on CARRA EAST

--- dataset/prepare_dataset.py
+++ dataset/prepare_dataset.py
@@ -11,6 +11,7 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 from netCDF4 import Dataset
+from pyproj import CRS, Transformer
 
 
 def read_and_merge(
@@ -76,12 +77,53 @@ def main():
 
     merged_df = pd.merge(full_lat_df, full_lon_df, left_index=True, right_index=True)
 
+    # SIC column to merged_df here then merge with temp dfs
+    new_columns = merged_df.columns.str.replace("lon", "sic")
+    merged_df = merged_df.assign(
+        **{
+            new_col: merged_df[col]
+            for new_col, col in zip(new_columns, merged_df.columns)
+        }
+    )
+
+    # Define CARRA grid constants
+
+    with Dataset(
+        f"{path_data}CARRA/2025/04/CARRA_extracted_sic_20250427.nc", "r"
+    ) as nc_carra_const:
+        carra_x = nc_carra_const["x"][:]
+        carra_y = nc_carra_const["y"][:]
+        proj_carra = vars(nc_carra_const["Lambert_Conformal"])
+
+    proj4_carra = f"+proj=lcc +lat_0={int(proj_carra['standard_parallel'])} +lon_0={int(proj_carra['longitude_of_central_meridian'])} +lat_1={int(proj_carra['latitude_of_projection_origin'])} +R={int(proj_carra['earth_radius'])} +y_0={proj_carra['false_northing']} +x_0={proj_carra['false_easting']}"
+
+    crs_wgs84 = CRS.from_epsg(4326)
+    crs_carra = CRS.from_proj4(proj4_carra)
+    transformer_to_carra = Transformer.from_crs(crs_wgs84, crs_carra)
+
+    for ts, row in merged_df.iterrows():
+        # Extract current CARRA SIC field
+        with Dataset(
+            f"{path_data}CARRA/{ts.year}/{ts.month:02d}/CARRA_extracted_sic_{ts.year}{ts.month:02d}{ts.day:02d}.nc"
+        ) as nc_carra:
+            current_sic = nc_carra["ci"][0]
+
+        for idx in row.index:
+            if idx.endswith("_sic") and not pd.isna(row[idx]):
+                row_lat = row[f"{idx[:-3]}lat"]
+                row_lon = row[f"{idx[:-3]}lon"]
+
+                row_x, row_y = transformer_to_carra.transform(row_lat, row_lon)
+
+                x_idx = (np.abs(carra_x - row_x)).argmin()
+                y_idx = (np.abs(carra_y - row_y)).argmin()
+
+                merged_df.at[ts, idx] = current_sic[y_idx, x_idx]
+
     temp_dfs = [full_temp_df, full_surf_df, full_sii_df, full_ice_df]
 
     for temp_df in temp_dfs:
         merged_df = pd.merge(merged_df, temp_df, left_index=True, right_index=True)
-
-    # TODO: Add SIC column to merged_df here
 
     buoy_indices = []
 
@@ -100,6 +142,7 @@ def main():
         surf_col = f"{buoy_idx}_temp_mlx_internal"
         sii_col = f"{buoy_idx}_temp_snow_ice_raw"
         ice_col = f"{buoy_idx}_temp_ice_raw"
+        sic_col = f"{buoy_idx}_sic"
 
         reshaped_data.append(
             pd.DataFrame(
@@ -110,6 +153,7 @@ def main():
                     "temp_surf": merged_df[surf_col],
                     "temp_snow_ice": merged_df[sii_col],
                     "temp_ice": merged_df[ice_col],
+                    "sic": merged_df[sic_col],
                 }
             ).assign(KVS_BUOY_IDX=buoy_idx)
         )
@@ -120,6 +164,9 @@ def main():
     )
 
     full_latlontemp_df.to_csv(f"{path_data}prepared_buoy_data.csv")
+
+    test_df = pd.read_csv(f"{path_data}prepared_buoy_data.csv", index_col=[0, 1])
+    print(test_df)
 
 
 if __name__ == "__main__":
