cc925cd9fa60984d9b31fd3d6fb28ee49a3fbe01
Are Frode Kvanum <arefk@met.no>
2025-11-12 16:01:29 +0100

    Pushing for gputest

--- src/ml_correction.py
+++ src/ml_correction.py
@@ -1,4 +1,5 @@
 import torch
+import os
 
 import pandas as pd
 import numpy as np
@@ -7,34 +8,9 @@ from tqdm import tqdm
 from torch import nn as nn
 from torch import functional as F
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, ParameterGrid
 from sklearn.preprocessing import StandardScaler
-
-# #### Prepare data
-
-path_data = "../dataset/"
-buoys_df = pd.read_csv(f"{path_data}prepared_buoy_data.csv", index_col=[0, 1])
-
-kvs_10 = buoys_df.loc['KVS_SvalMIZ_10'].dropna()
-
-X = kvs_10.drop(columns=['lat', 'lon', 'temp_air',
-                'temp_surf', 'temp_snow_ice', 'temp_ice'])
-y = kvs_10[['temp_air']]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-
-X_train, X_val, y_train, y_val = train_test_split(
-    X_train, y_train, test_size=.2)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_val = scaler.transform(X_val)
-X_test = scaler.transform(X_test)
-
-y_scaler = StandardScaler()
-y_train = y_scaler.fit_transform(y_train)
-y_val = y_scaler.transform(y_val)
-y_test = y_scaler.transform(y_test)
+from joblib import dump
 
 
 class BuoyDataset(torch.utils.data.Dataset):
@@ -47,19 +23,6 @@ class BuoyDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
-
-
-train_dataset = BuoyDataset(X_train, y_train)
-val_dataset = BuoyDataset(X_val, y_val)
-test_dataset = BuoyDataset(X_test, y_test)
-
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=64, shuffle=True)
-val_loader = torch.utils.data.DataLoader(
-    val_dataset, batch_size=64, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
-
-# #### Define model
 
 
 class SimpleMLP(nn.Module):
@@ -76,66 +39,175 @@ class SimpleMLP(nn.Module):
     def forward(self, x):
         return self.network(x)
 
-# #### training loop
+
+class LessSimpleMLP(nn.Module):
+    def __init__(self, in_size=1, out_size=1, hidden_sizes=[16, 32, 64]):
+        super().__init__()
+        layers = nn.ModuleList([])
+
+        for h_size in hidden_sizes:
+            layers.extend([
+                nn.Linear(in_size, h_size),
+                nn.BatchNorm1d(h_size),
+                nn.ReLU(),
+                nn.Dropout(0.1),
+            ])
+            in_size = h_size
+
+        for h_size in hidden_sizes[-2::-1]:
+            layers.extend([
+                nn.Linear(in_size, h_size),
+                nn.BatchNorm1d(h_size),
+                nn.ReLU(),
+                nn.Dropout(0.1),
+            ])
+            in_size = h_size
+
+        layers.append(nn.Linear(in_size, 1))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
 
 
-model = SimpleMLP(in_size=X.shape[1])
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer,
-    mode='min',
-    factor=0.5,
-    patience=10
-)
-criterion = nn.MSELoss()
+def add_cyclic_time(df):
+    df['hour_sin'] = np.sin(2 * np.pi * df['hour'] / 24)
+    df['hour_cos'] = np.cos(2 * np.pi * df['hour'] / 24)
 
-best_val_loss = float('inf')
-epochs = 100
+    df['day_sin'] = np.sin(2 * np.pi * df['doy'] / 365)
+    df['day_cos'] = np.cos(2 * np.pi * df['doy'] / 365)
 
-prog_bar = tqdm(range(epochs))
+    return df
 
-for epoch in prog_bar:
-    prog_bar.set_description(
-        desc=f"Epoch: {epoch+1}, lr: {optimizer.param_groups[0]["lr"]}")
-    model.train()
-    total_loss = []
-
-    # training
-    for X, y in train_loader:
-        optimizer.zero_grad()
-        preds = model(X)
-
-        loss = criterion(preds, y)
-        loss.backward()
-        optimizer.step()
-        total_loss.append(loss.item())
-
-    # Validation
-    model.eval()
-    total_val_loss = []
-    with torch.no_grad():
-        for X, y in val_loader:
-            val_pred = model(X)
-            total_val_loss.append(criterion(val_pred, y).item())
-
-    prog_bar.set_postfix({'loss': f"{np.mean(total_loss):.4f}",
-                         "val_loss": f"{np.mean(total_val_loss):.4f}"})
-    scheduler.step(np.mean(total_val_loss))
+# #### Prepare data
 
 
-model.eval()
-print("\n-----\n")
+def main():
+    path_data = "../dataset/"
+    buoys_df = pd.read_csv(
+        f"{path_data}prepared_buoy_data.csv", index_col=[0, 1])
 
-with torch.no_grad():
+    # Keep buoy10 for testing
+    kvs = buoys_df.loc[['KVS_SvalMIZ_03',
+                        'KVS_SvalMIZ_07']].dropna().reset_index(level=0)
 
-    test_rmse = []
-    for X, y in test_loader:
-        test_pred = y_scaler.inverse_transform(model(X))
+    kvs = kvs.set_index(pd.to_datetime(kvs.index))
+    kvs['residuals'] = kvs['temp_air'] - kvs['arome_t2m']
+    kvs['hour'] = kvs.index.hour
+    kvs['doy'] = kvs.index.day_of_year
 
-        y = y_scaler.inverse_transform(y)
+    X = kvs[['arome_t2m', 'sic', 'hour', 'doy']]
+    y = kvs[['residuals']]
 
-        for i, pred in enumerate(test_pred):
-            se = (pred - y[i])**2
-            test_rmse.append(np.sqrt(se))
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, test_size=.2, random_state=1913)
 
-print(f"{np.mean(test_rmse)}")
+    X_train_time = X_train[['hour', 'doy']]
+    X_train = X_train.drop(['hour', 'doy'], axis=1)
+
+    X_val_time = X_val[['hour', 'doy']]
+    X_val = X_val.drop(['hour', 'doy'], axis=1)
+
+    X_train_time = add_cyclic_time(X_train_time.copy())
+    X_val_time = add_cyclic_time(X_val_time.copy())
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_val = scaler.transform(X_val)
+
+    X_train = np.concatenate(
+        [X_train, X_train_time[['hour_sin', 'hour_cos', 'day_sin', 'day_cos']]], axis=1)
+
+    X_val = np.concatenate(
+        [X_val, X_val_time[['hour_sin', 'hour_cos', 'day_sin', 'day_cos']]], axis=1)
+
+    train_dataset = BuoyDataset(X_train, y_train.to_numpy())
+    val_dataset = BuoyDataset(X_val, y_val.to_numpy())
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=64, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=64, shuffle=True)
+
+    # #### Define model
+
+    # #### training loop
+
+    model = LessSimpleMLP(in_size=X_train.shape[1])
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        mode='min',
+        factor=0.5,
+        patience=10
+    )
+    criterion = nn.MSELoss()
+
+    best_params = None
+    best_loss = float('inf')
+
+    param_grid = {
+        'lr': [0.001, 0.01, 0.1],
+        'epochs': [20, 100, 200],
+        'hidden_sizes': [[16, 32], [16, 32, 64], [16, 32, 64, 128], [32, 64, 128], [32, 64]]
+    }
+
+    for params in ParameterGrid(param_grid):
+
+        model = LessSimpleMLP(
+            in_size=X_train.shape[1], hidden_sizes=params['hidden_sizes'])
+        optimizer = torch.optim.Adam(model.parameters(), lr=params['lr'])
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode='min',
+            factor=0.5,
+            patience=10
+        )
+
+        prog_bar = tqdm(range(params['epochs']))
+
+        for epoch in prog_bar:
+            prog_bar.set_description(
+                desc=f"Epoch: {epoch+1}, lr: {optimizer.param_groups[0]["lr"]}")
+            model.train()
+            total_loss = []
+
+            # training
+            for X, y in train_loader:
+                optimizer.zero_grad()
+                preds = model(X)
+
+                loss = criterion(preds, y)
+                loss.backward()
+                optimizer.step()
+                total_loss.append(loss.item())
+
+            # Validation
+            model.eval()
+            total_val_loss = []
+            with torch.no_grad():
+                for X, y in val_loader:
+                    val_pred = model(X)
+                    total_val_loss.append(criterion(val_pred, y).item())
+
+            prog_bar.set_postfix({'loss': f"{np.mean(total_loss):.4f}",
+                                  "val_loss": f"{np.mean(total_val_loss):.4f}"})
+            scheduler.step(np.mean(total_val_loss))
+
+        if np.mean(total_val_loss) < best_loss:
+            best_loss = np.mean(total_val_loss)
+            best_params = params
+
+    os.makedirs('models/', exist_ok=True)
+
+    print(best_loss)
+    print(best_params)
+
+    exit()
+
+    torch.save(model, 'models/diamond_dnn.pt')
+    dump(scaler, 'models/X_train_scaler.bin', compress=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/train_xgboost.py
@@ -0,0 +1,77 @@
+
+import os
+
+import pandas as pd
+import numpy as np
+
+from xgboost import XGBRegressor
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+from ml_correction import add_cyclic_time
+
+
+def main():
+    path_data = "../dataset/"
+    buoys_df = pd.read_csv(
+        f"{path_data}prepared_buoy_data.csv", index_col=[0, 1])
+
+    # Keep buoy10 for testing
+    kvs = buoys_df.loc[['KVS_SvalMIZ_03',
+                        'KVS_SvalMIZ_07']].dropna().reset_index(level=0)
+
+    kvs = kvs.set_index(pd.to_datetime(kvs.index))
+    kvs['residuals'] = kvs['temp_air'] - kvs['arome_t2m']
+    kvs['hour'] = kvs.index.hour
+    kvs['doy'] = kvs.index.day_of_year
+
+    X = kvs[['arome_t2m', 'sic', 'hour', 'doy']]
+    y = kvs[['residuals']]
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, test_size=.2, random_state=1913)
+
+    X_train_time = X_train[['hour', 'doy']]
+    X_train = X_train.drop(['hour', 'doy'], axis=1)
+
+    X_val_time = X_val[['hour', 'doy']]
+    X_val = X_val.drop(['hour', 'doy'], axis=1)
+
+    X_train_time = add_cyclic_time(X_train_time.copy())
+    X_val_time = add_cyclic_time(X_val_time.copy())
+
+    X_train = np.concatenate(
+        [X_train, X_train_time[['hour_sin', 'hour_cos', 'day_sin', 'day_cos']]], axis=1)
+
+    X_val = np.concatenate(
+        [X_val, X_val_time[['hour_sin', 'hour_cos', 'day_sin', 'day_cos']]], axis=1)
+
+    param_grid = {
+        'n_estimators': [100, 500],
+        'max_depth': [3, 6, 9, 12],
+        'learning_rate': [0.01, 0.1, 0.2],
+        'subsample': [0.8, 1.0],
+        'colsample_bytree': [0.6, 0.8, 1.0]
+    }
+
+    model = XGBRegressor(objective='reg:squarederror',
+                         n_estimators=100, random_state=1913)
+
+    grid_search = GridSearchCV(
+        estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=1
+    )
+    grid_search.fit(X_train, y_train, eval_set=[(X_val, y_val)], verbose=False)
+
+    print("Best params:", grid_search.best_params_)
+    model = grid_search.best_estimator_
+    preds = model.predict(X_val)
+    print(preds[:5])
+    print(y_val[:5])
+
+    os.makedirs('models/', exist_ok=True)
+
+    model.save_model("models/xgboost_model.json")
+
+
+if __name__ == "__main__":
+    main()
