10118d2dfd350004b736bc5d0f20552336aaf18c
Are Frode Kvanum <arefk@met.no>
2025-11-17 17:36:15 +0100

    Push model
    xgboost done

--- src/bayesian_linreg.py
+++ src/bayesian_linreg.py
@@ -1,12 +1,22 @@
+import numpyro
+import torch
+
+import seaborn as sns
 import numpy as np
 import pandas as pd
-import pyro
-import torch
+
+from numpyro.diagnostics import hpdi
+from numpyro.infer import MCMC, NUTS
+from jax import random
+
+from jax import numpy as jnp
+from numpyro import distributions as dist
+from matplotlib import pyplot as plt
 
 from ml_correction import add_cyclic_time
 
 
-def bayesian_model(x_data_, y_data_):
+def old_bayesian_model(x_data_, y_data_):
     s = pyro.sample("s", pyro.distributions.Normal(0, 1))
 
     b = pyro.sample("b", pyro.distributions.Normal(0., 1000.))
@@ -21,6 +31,18 @@ def bayesian_model(x_data_, y_data_):
             loc=mean, scale=torch.sqrt(1. / theta)), obs=y_data_)
 
     return mean
+
+
+def bayesian_model(x_data_, y_data_):
+    a = numpyro.sample("a", dist.Normal(0.0, 0.2))
+
+    b = numpyro.sample("b", dist.Normal(0.0, 0.5))
+
+    M = b * x_data_
+
+    sigma = numpyro.sample("sigma", dist.Exponential(1.0))
+    mu = a + M
+    numpyro.sample("obs", dist.Normal(mu, sigma), obs=y_data_)
 
 
 def train(x_data, y_data, model, num_iterations=1500):
@@ -45,8 +67,30 @@ def train(x_data, y_data, model, num_iterations=1500):
     return param
 
 
+def standardize(x):
+    return (x - x.mean()) / x.std()
+
+
+def plot_regression(x, y, y_mean, y_hpdi):
+    idx = jnp.argsort(x)
+    arome_t2m = x[idx]
+    mean = y_mean[idx]
+    hpdi = y_hpdi[:, idx]
+    residuals = y[idx]
+
+    # Plot
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
+    ax.plot(arome_t2m, mean)
+    ax.plot(arome_t2m, residuals, "o")
+    ax.fill_between(arome_t2m, hpdi[0], hpdi[1], alpha=.3, interpolate=True)
+    return fig, ax
+
+
 def main():
+    plt.style.use('bmh')
+
     path_data = "../dataset/"
+    path_figs = "../figures/"
     buoys_df = pd.read_csv(
         f"{path_data}prepared_buoy_data.csv", index_col=[0, 1])
 
@@ -59,7 +103,7 @@ def main():
     kvs['hour'] = kvs.index.hour
     kvs['doy'] = kvs.index.day_of_year
 
-    X = kvs[['arome_t2m', 'sic', 'hour', 'doy']]
+    X = kvs[['KVS_BUOY_IDX', 'arome_t2m', 'sic', 'hour', 'doy']]
     y = kvs[['residuals']]
 
     X_time = X[['hour', 'doy']]
@@ -67,11 +111,61 @@ def main():
 
     X_time = add_cyclic_time(X_time.copy())
 
-    X = np.concatenate(
+    X = pd.concat(
         [X, X_time[['hour_sin', 'hour_cos', 'day_sin', 'day_cos']]], axis=1)
 
-    idxs = ['arome_t2m', 'sic', 'hour_sin', 'hour_cos', 'day_sin', 'day_cos']
+    idxs = ['KVS_BUOY_IDX', 'arome_t2m', 'sic',
+            'hour_sin', 'hour_cos', 'day_sin', 'day_cos', 'residuals']
 
+    processed_kvs = pd.concat([X, y], axis=1)
+
+    # sns.pairplot(processed_kvs, x_vars=idxs, y_vars=idxs,
+    # hue="doy", palette='husl')
+    # plt.savefig(f'{path_figs}pairplot_alldata.png')
+
+    # sns.regplot(x='day_sin', y='residuals', data=processed_kvs)
+    # plt.savefig(f"{path_figs}regplot_dsin_residuals.png")
+
+    X["arome_t2m_scaled"] = X.arome_t2m.pipe(standardize)
+    y.loc[:, 'residuals_scaled'] = y.residuals.pipe(standardize)
+
+    # Start from source of randomness
+    rng_key = random.PRNGKey(1913)
+    rng_key, rng_key_ = random.split(rng_key)
+
+    # Run NUTS
+    kernel = NUTS(bayesian_model)
+
+    num_samples = 2000
+    mcmc = MCMC(kernel, num_warmup=1000, num_samples=num_samples)
+    mcmc.run(
+        rng_key_,
+        X['arome_t2m_scaled'].values,
+        y['residuals_scaled'].values
+    )
+
+    mcmc.print_summary()
+    samples_1 = mcmc.get_samples()
+
+    # Compute empirical posterior over mu
+    posterior_mu = (
+        jnp.expand_dims(samples_1["a"], -1)
+        + jnp.expand_dims(samples_1["b"], -1) * X["arome_t2m_scaled"].values
+    )
+
+    mean_mu = jnp.mean(posterior_mu, axis=0)
+    hpdi_mu = hpdi(posterior_mu, 0.9)
+    fig, ax = plot_regression(
+        X['arome_t2m_scaled'].values, y['residuals_scaled'].values, mean_mu, hpdi_mu)
+
+    ax.set(
+        xlabel="Forecasted T2M",
+        ylabel="Resdiuals",
+        title="Regression line with 90% CI"
+    )
+
+    fig.savefig(f"{path_figs}Posterior_dist_over_regression_parameters.png")
+    ''' 
     x_data = {}
     for idx, val in enumerate(idxs):
         x_data[idxs[idx]] = torch.tensor(
@@ -91,6 +185,7 @@ def main():
     print(f"-"*len(header))
     for name, value in param['vi_parameters']['params'].items():
         print(f"{name:<25}: {value.detach().numpy()}")
+    '''
 
 
 if __name__ == "__main__":
